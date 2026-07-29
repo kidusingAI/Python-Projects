9a418308a53c1dd7d76bd34f3c1de29d66347039
kidusingAI <yosef.shamoug@Gmail.com>
2026-07-30 01:27:22 +0800

    Delete phonenumber.py

--- phonenumber.py
+++
@@ -1,6 +0,0 @@
-#to create a phone number
-
-def phonenumber(countrycode,areacode,first3,last3):
-  print(f"+{countrycode}({areacode}) {first3}-{last3}")
-
-phonenumber(countrycode=1,areacode=234,first3=567,last3=890)
